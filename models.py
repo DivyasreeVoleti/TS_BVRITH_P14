8b3294ca9139d48371fa9887ff8e69b4fc70765d
Sravani478 <14wh1a0478@bvrithyderabad.edu.in>
2017-03-24 09:16:09 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+class Campus_drive(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):              
+        return self.name
+
+class Company(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):              
+        return self.name
+
+class student_id(models.Model):
+    student_id = models.ForeignKey(student_id, on_delete=models.CASCADE)
+    date_joined = models.DateField()
